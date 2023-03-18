90bdcb039c075dcb141c97e629ccd14901813ee1
Oleksii Ivashchenko <ivash.lex@gmail.com>
2023-03-18 16:16:59 +0100

    Test tweaks, aded Readme file, Added requirements.

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# playwright-python-boilerplate
+# Playwright Test Suite
+
+Created by: **Oleksii Ivashchenko** [Linkedin](https://www.linkedin.com/in/oivashchenko/)
+
+This project contains a test suite using [Playwright](https://playwright.dev/) and [pytest](https://docs.pytest.org/en/latest/) for end-to-end testing of a web application.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.7 or later
+- A compatible browser (Chromium, Firefox, or WebKit)
+
+### Installation
+
+Clone the repository:
+
+`git clone https://github.com/yourusername/playwright-test-suite.git`
+`cd playwright-test-suite`
+
+
+Install the required dependencies: `pip install -r requirements.txt`
+
+Install the Playwright browser binaries: `playwright install`
+
+## Running the Tests
+
+To run the entire test suite, execute the following command: `pytest`
+
+For more options and configurations, refer to the `pytest.ini` file in the project's root directory.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details.
--- tests/test_login.py
+++ tests/test_login.py
@@ -3,18 +3,27 @@ import pytest
 from src.config import TEST_PASSWORD, TEST_USERNAME, BASE_URL
 from src.pages.inventory_page import InventoryPage
 
-@pytest.mark.parametrize("username", TEST_USERNAME)
+@pytest.mark.parametrize("username", [TEST_USERNAME, "problem_user"])
 def test_valid_login(login_page, username):
     login_page.go_to_url(BASE_URL)
-    login_page.login(TEST_USERNAME, TEST_PASSWORD)
+    login_page.login(username, TEST_PASSWORD)
 
     # Verify that the user is logged in and redirected to the correct page
     inventory_page = InventoryPage(login_page.page)
     assert inventory_page.is_page_loaded(), f"User '{username}' should be logged in."
 
-def test_invalid_login(login_page):
+@pytest.mark.parametrize("username", ["invalid_user", "locked_out_user", "!@#$%$!#%@", " "])
+def test_invalid_login(login_page, username):
     login_page.go_to_url(BASE_URL)
-    login_page.login("invalid_user", "wrong_password")
+    login_page.login(username, "wrong_password")
+
+    # Verify that an error message is displayed
+    assert login_page.is_error_message_visible(), "Error message should be visible for invalid login."
+    assert login_page.is_page_loaded(), "Login page should be be still visible."
+
+def test_invalid_login_no_password(login_page):
+    login_page.go_to_url(BASE_URL)
+    login_page.login(TEST_USERNAME, "")
 
     # Verify that an error message is displayed
     assert login_page.is_error_message_visible(), "Error message should be visible for invalid login."
